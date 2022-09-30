b7365f4b23d3f0cffc63a20ef040cdcfdde557e0
Emiliano <emiliano.hotes@despegar.com>
2022-09-30 19:00:04 -0300

    formatting date

--- expense-tracker-app/src/components/ExpenseItem.tsx
+++ expense-tracker-app/src/components/ExpenseItem.tsx
@@ -8,13 +8,13 @@ type Props = {
 };
 
 const ExpenseItem = ({ title, amount, date }: Props) => {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-
   return (
     <div className="expense-item">
-      <div>{date.toDateString()}</div>
+      <div>
+        <div>{date.toLocaleString("es-AR", { month: "long" })}</div>
+        <div>{date.toLocaleString("es-AR", { day: "2-digit" })}</div>
+        <div>{date.getFullYear()}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
