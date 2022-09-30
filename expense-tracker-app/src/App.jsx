ca18026da4f960c24fb7eb40d9f55e24daaf0816
Emiliano <emiliano.hotes@despegar.com>
2022-09-30 18:17:23 -0300

    Added mock data to ExpenseItem component

--- expense-tracker-app/src/App.jsx
+++ expense-tracker-app/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
-
       <ExpenseItem />
     </div>
   );
--- expense-tracker-app/src/components/ExpenseItem.jsx
+++ expense-tracker-app/src/components/ExpenseItem.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ExpenseItem = () => {
-  return <div>ExpenseItem</div>;
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
