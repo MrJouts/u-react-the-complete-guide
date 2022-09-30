666ab00ca32e1b5e750312743c84351f84ed92b5
Emiliano <emiliano.hotes@despegar.com>
2022-09-30 18:37:29 -0300

    added variables to ExpenseItem

--- expense-tracker-app/src/components/ExpenseItem.jsx
+++ expense-tracker-app/src/components/ExpenseItem.jsx
@@ -3,12 +3,16 @@ import React from "react";
 import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
