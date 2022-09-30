b286877a9cdc854de52df63f080fff8287d22e1c
Emiliano <emiliano.hotes@despegar.com>
2022-09-30 18:09:37 -0300

    created ExpenseItem component

---
+++ expense-tracker-app/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
