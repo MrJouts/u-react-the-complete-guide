133d34fd857853e663a1424e8b832ab6cbc13d5e
Emiliano <emiliano.hotes@despegar.com>
2022-09-30 18:51:03 -0300

    added data to ExpenseItem via props

--- expense-tracker-app/src/components/ExpenseItem.tsx
+++ expense-tracker-app/src/components/ExpenseItem.tsx
@@ -1,18 +1,23 @@
-import React from "react";
-
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+type Props = {
+  id?: string;
+  title: string;
+  amount: number;
+  date: Date;
+};
+
+const ExpenseItem = ({ title, amount, date }: Props) => {
   const expenseDate = new Date(2021, 2, 28);
   const expenseTitle = "Car Insurance";
   const expenseAmount = 294.67;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
+      <div>{date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
