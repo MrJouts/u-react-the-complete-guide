6a05ffae814dfb1a2e03f788899673b32303933c
Emiliano <emiliano.hotes@despegar.com>
2022-09-30 23:43:55 -0300

    added state to application

--- expense-tracker-app/src/components/Expenses/ExpenseItem.tsx
+++ expense-tracker-app/src/components/Expenses/ExpenseItem.tsx
@@ -1,6 +1,7 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import { useState } from "react";
 
 type Props = {
   id?: string;
@@ -10,12 +11,17 @@ type Props = {
 };
 
 const ExpenseItem = ({ title, amount, date }: Props) => {
+  const [cardTitle, setCardTitle] = useState(title);
+
+  const clickHandler = () => setCardTitle("Updated!");
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{cardTitle}</h2>
         <div className="expense-item__price">${amount}</div>
+        <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
   );
