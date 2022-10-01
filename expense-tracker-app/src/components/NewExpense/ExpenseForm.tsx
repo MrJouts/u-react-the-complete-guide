5474d4fb421fb33476199503a8b6c98ea909d55e
Emiliano <emiliano.hotes@despegar.com>
2022-10-01 00:39:34 -0300

    clear inputs after submit

--- expense-tracker-app/src/components/NewExpense/ExpenseForm.tsx
+++ expense-tracker-app/src/components/NewExpense/ExpenseForm.tsx
@@ -33,6 +33,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -40,7 +43,12 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="title">Title</label>
-          <input id="title" type="text" onChange={titleChangeHandler} />
+          <input
+            id="title"
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label htmlFor="amount">Amount</label>
@@ -49,6 +57,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -59,6 +68,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
