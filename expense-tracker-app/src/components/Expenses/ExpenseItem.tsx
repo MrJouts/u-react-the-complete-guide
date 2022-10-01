6cd0b04d905d044a89d321a70f189636784768ec
Emiliano <emiliano.hotes@despegar.com>
2022-10-01 11:06:31 -0300

    removed button from ExpenseItem

--- expense-tracker-app/src/components/Expenses/ExpenseItem.tsx
+++ expense-tracker-app/src/components/Expenses/ExpenseItem.tsx
@@ -1,7 +1,6 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import { useState } from "react";
 
 type Props = {
   id?: string;
@@ -11,17 +10,12 @@ type Props = {
 };
 
 const ExpenseItem = ({ title, amount, date }: Props) => {
-  const [cardTitle, setCardTitle] = useState(title);
-
-  const clickHandler = () => setCardTitle("Updated!");
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{cardTitle}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
-        <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
   );
