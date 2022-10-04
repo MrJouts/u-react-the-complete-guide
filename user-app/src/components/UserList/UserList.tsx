a0ce510816b94b7f2fa9dd4ec5610de694953a21
Emiliano <emiliano.hotes@despegar.com>
2022-10-04 19:05:30 -0300

    added UserList component}

---
+++ user-app/src/components/UserList/UserList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./UserList.module.scss";
+
+const UserList = () => {
+  return <div className={styles.test}>UserList</div>;
+};
+
+export default UserList;
