7b4a908961cecfeea0336fce77b4c57982ecc044
Emiliano <emiliano.hotes@despegar.com>
2022-10-04 19:16:04 -0300

    added users mock

--- user-app/src/App.tsx
+++ user-app/src/App.tsx
@@ -2,11 +2,35 @@ import { useState } from "react";
 import "./App.css";
 import UserList from "./components/UserList/UserList";
 
+const DEFAULT_USERS = [
+  {
+    id: 1,
+    name: "Carlos",
+    age: 45,
+  },
+  {
+    id: 2,
+    name: "Marcela",
+    age: 24,
+  },
+  {
+    id: 2,
+    name: "Gonzalo",
+    age: 33,
+  },
+];
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState(DEFAULT_USERS);
 
   return (
-    <div className="App">
+    <div className="user-app">
+      {users.map(({ name, age }) => (
+        <div>
+          <span>{name}</span>
+          <span>{age}</span>
+        </div>
+      ))}
       <UserList />
     </div>
   );
--- user-app/src/components/UserList/UserList.tsx
+++ user-app/src/components/UserList/UserList.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import styles from "./UserList.module.scss";
 
 const UserList = () => {
