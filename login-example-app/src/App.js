d15e5e638e8dbdf41726c9cc10f1624abfeaecb3
Emiliano <emiliano.hotes@despegar.com>
2022-10-06 17:46:59 -0300

    added loggedIn status to localStorage

--- login-example-app/src/App.js
+++ login-example-app/src/App.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const isLoggedInLS = localStorage.getItem("isLoggedIn");
+
+    if (isLoggedInLS === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", "1");
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem("isLoggedIn");
   };
 
   return (
