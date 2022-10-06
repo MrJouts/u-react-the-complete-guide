e93eff0c2c9367d0c00e2765275ede0329a30121
Emiliano <emiliano.hotes@despegar.com>
2022-10-06 11:05:49 -0300

    removed services folder

--- user-app/src/components/services/formValidation.service.ts
+++
@@ -1,8 +0,0 @@
-export const validateInput = (input, data) => {
-  switch (input) {
-    case "name":
-      if (data.trim().length === 0) {
-        return "Name cannot be empty";
-      }
-  }
-};
