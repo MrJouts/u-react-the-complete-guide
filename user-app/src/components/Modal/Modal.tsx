d98369131e3e7c67ca6cc68b6020e6a5292b20a1
Emiliano <emiliano.hotes@despegar.com>
2022-10-06 12:10:25 -0300

    added react portals to show modal

--- user-app/src/components/Modal/Modal.tsx
+++ user-app/src/components/Modal/Modal.tsx
@@ -1,4 +1,5 @@
-import { MouseEventHandler } from "react";
+import { MouseEventHandler, ReactElement, ReactPortal } from "react";
+import ReactDOM from "react-dom";
 import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
 import styles from "./Modal.module.scss";
@@ -9,25 +10,55 @@ type Props = {
   onClick: React.MouseEventHandler;
 };
 
+type BackdropProps = {
+  onClick: React.MouseEventHandler;
+};
+
+type ModalOverlayProps = {
+  title: string;
+  message: string;
+  onClick: React.MouseEventHandler;
+};
+
+const Backdrop = ({ onClick }: BackdropProps): JSX.Element => (
+  <div className={styles.modalBackdrop} onClick={onClick}></div>
+);
+
+const ModalOverlay = ({
+  title,
+  message,
+  onClick,
+}: ModalOverlayProps): JSX.Element => (
+  <Card
+    className={styles.modalCard}
+    onClick={(e: Event) => {
+      e.stopPropagation();
+      return false;
+    }}
+  >
+    <div className="cardContent">
+      <h2 className={styles.modalTitle}>{title}</h2>
+      <div className={styles.modalContent}>{message}</div>
+      <Button type="button" onClick={onClick}>
+        Close
+      </Button>
+    </div>
+  </Card>
+);
+
 const Modal = ({ title, message, onClick }: Props) => {
   return (
-    <div className={styles.modalBackdrop} onClick={onClick}>
-      <Card
-        className={styles.modalCard}
-        onClick={(e: Event) => {
-          e.stopPropagation();
-          return false;
-        }}
-      >
-        <div className="cardContent">
-          <h2 className={styles.modalTitle}>{title}</h2>
-          <div className={styles.modalContent}>{message}</div>
-          <Button type="button" onClick={onClick}>
-            Close
-          </Button>
-        </div>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={onClick} />,
+        document.getElementById("backdrop-root")!
+      )}
+
+      {ReactDOM.createPortal(
+        <ModalOverlay title={title} message={message} onClick={onClick} />,
+        document.getElementById("modal-overlay")!
+      )}
+    </>
   );
 };
 
