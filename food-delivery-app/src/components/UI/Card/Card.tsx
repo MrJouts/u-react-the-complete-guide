ee9e7be22ed28e33d29d879240ece7189e79e30d
Emiliano <emiliano.hotes@despegar.com>
2022-10-10 20:18:05 -0300

    Added Card component

---
+++ food-delivery-app/src/components/UI/Card/Card.tsx
@@ -0,0 +1,7 @@
+import classes from "./Card.module.scss";
+
+const Card = () => {
+  return <div className={classes.card}>Card content</div>;
+};
+
+export default Card;
--- food-delivery-app/src/pages/Home.tsx
+++ food-delivery-app/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header/Header";
+import Card from "../components/UI/Card/Card";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <p>Home content</p>
+      <Card />
     </div>
   );
 };
