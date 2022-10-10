c6e852ff34583983a0968bfc460000a640ab36de
Emiliano <emiliano.hotes@despegar.com>
2022-10-10 20:14:56 -0300

    added HeaderCartButton component

--- food-delivery-app/src/components/Header/Header.tsx
+++ food-delivery-app/src/components/Header/Header.tsx
@@ -1,7 +1,13 @@
 import classes from "./Header.module.scss";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
-  return <div className={classes.header}>Header</div>;
+  return (
+    <div className={classes.header}>
+      <h1>React Meals</h1>
+      <HeaderCartButton />
+    </div>
+  );
 };
 
 export default Header;
---
+++ food-delivery-app/src/components/Header/HeaderCartButton.tsx
@@ -0,0 +1,7 @@
+import classes from "./HeaderCartButton.module.scss";
+
+const HeaderCartButton = () => {
+  return <button className={classes.button}>View cart</button>;
+};
+
+export default HeaderCartButton;
