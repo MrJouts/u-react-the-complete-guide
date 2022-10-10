0a45c8220df7f2a3f594a98c0f0196cecfa6ece5
Emiliano <emiliano.hotes@despegar.com>
2022-10-10 20:38:11 -0300

    added image to header

--- food-delivery-app/src/components/Header/Header.tsx
+++ food-delivery-app/src/components/Header/Header.tsx
@@ -1,12 +1,19 @@
 import classes from "./Header.module.scss";
 import HeaderCartButton from "./HeaderCartButton";
 
+import mealsImage from "../../assets/meals.jpg";
+
 const Header = () => {
   return (
-    <div className={classes.header}>
-      <h1>React Meals</h1>
-      <HeaderCartButton />
-    </div>
+    <>
+      <div className={classes.header}>
+        <h2>React Meals</h2>
+        <HeaderCartButton />
+      </div>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="meals" />
+      </div>
+    </>
   );
 };
 
--- food-delivery-app/src/components/Header/HeaderCartButton.tsx
+++ food-delivery-app/src/components/Header/HeaderCartButton.tsx
@@ -1,7 +1,7 @@
 import classes from "./HeaderCartButton.module.scss";
 
 const HeaderCartButton = () => {
-  return <button className={classes.button}>Your Cart cart</button>;
+  return <button className={classes.button}>Your Cart</button>;
 };
 
 export default HeaderCartButton;
