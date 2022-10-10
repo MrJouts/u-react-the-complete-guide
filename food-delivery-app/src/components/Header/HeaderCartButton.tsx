523a12d65105209b15d18725c3202efdd3474536
Emiliano <emiliano.hotes@despegar.com>
2022-10-10 20:23:00 -0300

    added header styles

--- food-delivery-app/src/components/Header/HeaderCartButton.tsx
+++ food-delivery-app/src/components/Header/HeaderCartButton.tsx
@@ -1,7 +1,7 @@
 import classes from "./HeaderCartButton.module.scss";
 
 const HeaderCartButton = () => {
-  return <button className={classes.button}>View cart</button>;
+  return <button className={classes.button}>Your Cart cart</button>;
 };
 
 export default HeaderCartButton;
