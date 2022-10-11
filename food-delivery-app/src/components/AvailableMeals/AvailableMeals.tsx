a21ed3ec9453ab825c7afd94840774d7b9e24b9b
Emiliano <emiliano.hotes@despegar.com>
2022-10-11 18:18:17 -0300

    added mealItem component

--- food-delivery-app/src/components/AvailableMeals/AvailableMeals.tsx
+++ food-delivery-app/src/components/AvailableMeals/AvailableMeals.tsx
@@ -1,7 +1,13 @@
+import MealItem from "../MealItem/MealItem";
 import classes from "./AvailableMeals.module.scss";
 
 const AvailableMeals = () => {
-  return <div className={classes.meals}>AvailableMeals</div>;
+  return (
+    <div className={classes.meals}>
+      AvailableMeals
+      <MealItem />
+    </div>
+  );
 };
 
 export default AvailableMeals;
---
+++ food-delivery-app/src/components/MealItem/MealItem.tsx
@@ -0,0 +1,7 @@
+import classes from "MealItem.module.scss";
+
+const MealItem = () => {
+  return <div>MealItem</div>;
+};
+
+export default MealItem;
