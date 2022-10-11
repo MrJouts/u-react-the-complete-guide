8a3461e7bd91ef44d1379b7195d4a38d9ae264fb
Emiliano <emiliano.hotes@despegar.com>
2022-10-11 20:04:26 -0300

    added functionality to MealInputForm

--- food-delivery-app/src/components/MealItemForm/MealItemForm.tsx
+++ food-delivery-app/src/components/MealItemForm/MealItemForm.tsx
@@ -1,11 +1,34 @@
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import Input from "../UI/Input/Input";
 import classes from "./MealItemForm.module.scss";
 
 const MealItemForm = () => {
+  const [amount, setAmount] = useState<string>("1");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    // agrego un objeto meal al carrito
+    // tener en cuenta la quantity
+    console.log("meal added...", amount);
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setAmount(e.target.value);
+    console.log("amount", amount);
+  };
+
   return (
-    <form className={classes.form}>
-      <Input id="amount" label="Amount" type="number" />
-      <button>+ Add</button>
+    <form className={classes.form} onSubmit={handleSubmit}>
+      <Input
+        id="amount"
+        label="Amount"
+        type="number"
+        value={amount}
+        onChange={handleInputChange}
+      />
+      <button type="submit">+ Add</button>
     </form>
   );
 };
--- food-delivery-app/src/components/UI/Input/Input.tsx
+++ food-delivery-app/src/components/UI/Input/Input.tsx
@@ -5,14 +5,15 @@ type Props = {
   id: string;
   label: string;
   type: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
 };
 
-const Input = ({ id, label, type }: Props) => {
-  const [value, setValue] = useState(1);
+const Input = ({ id, label, type, value, onChange }: Props) => {
   return (
     <div className={classes.input}>
       <label htmlFor={id}>{label}</label>
-      <input id={id} type={type} value={value} />
+      <input id={id} type={type} value={value} onChange={onChange} />
     </div>
   );
 };
