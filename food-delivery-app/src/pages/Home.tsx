35933cc98338bd2df8927f3644756dc2ac3511ed
Emiliano <emiliano.hotes@despegar.com>
2022-10-11 18:27:04 -0300

    added meals to Home

--- food-delivery-app/src/pages/Home.tsx
+++ food-delivery-app/src/pages/Home.tsx
@@ -2,8 +2,13 @@ import AvailableMeals from "../components/AvailableMeals/AvailableMeals";
 import Header from "../components/Header/Header";
 import MealsSummary from "../components/MealsSummary/MealsSummary";
 import Card from "../components/UI/Card/Card";
+import { meal } from "../types";
 
-const Home = () => {
+type Props = {
+  meals: meal[];
+};
+
+const Home = ({ meals }: Props) => {
   return (
     <div>
       <Header />
