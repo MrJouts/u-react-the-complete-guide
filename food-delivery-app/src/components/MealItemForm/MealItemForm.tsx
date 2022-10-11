8e716799a4263a4b68437e455830b9312b9c156e
Emiliano <emiliano.hotes@despegar.com>
2022-10-11 18:38:44 -0300

    added formImput styles

--- food-delivery-app/src/components/MealItemForm/MealItemForm.tsx
+++ food-delivery-app/src/components/MealItemForm/MealItemForm.tsx
@@ -1,7 +1,15 @@
 import classes from "./MealItemForm.module.scss";
 
 const MealItemForm = () => {
-  return <div>MealItemForm</div>;
+  return (
+    <form className={classes.form}>
+      <div>
+        <label htmlFor="amount">Amount</label>
+        <input type="number" />
+      </div>
+      <button>+ Add</button>
+    </form>
+  );
 };
 
 export default MealItemForm;
