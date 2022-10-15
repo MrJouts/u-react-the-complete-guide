049ad4ef28db40c504ed58bfb69a233b413a1532
Emiliano <emiliano.hotes@despegar.com>
2022-10-15 15:55:16 -0300

    added cartItems from contexto to Cart component

--- food-delivery-app/src/components/Cart/Cart.tsx
+++ food-delivery-app/src/components/Cart/Cart.tsx
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import { CartItem as ICartItem } from "../../types";
 import { formatPrice } from "../../utils/format-price";
 import Modal from "../UI/Modal";
@@ -5,34 +7,34 @@ import classes from "./Cart.module.scss";
 import CartItem from "./CartItem";
 
 type Props = {
-  cartItems: ICartItem[] | null;
   onClose: () => void;
 };
 
-const Cart = ({ cartItems, onClose }: Props) => {
-  let total = 0;
+const Cart = ({ onClose }: Props) => {
+  const cartCtx = useContext(CartContext);
+  const hasItems: boolean = cartCtx.totalAmount > 0;
 
-  cartItems?.forEach((cart) => {
-    total += cart.price * cart.amount;
-  });
+  const cartItemRemoveHandler = (id: string) => {};
+
+  const cartItemAddHandler = (item: ICartItem) => {};
 
   return (
     <Modal onClose={onClose}>
       <div className={classes["cart-items"]}>
-        {cartItems?.map((cart) => (
+        {cartCtx.items.map((cart) => (
           <CartItem key={cart.name} {...cart} />
         ))}
       </div>
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>{formatPrice(total)}</span>
+        <span>{formatPrice(cartCtx.totalAmount)}</span>
       </div>
 
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
