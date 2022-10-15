229be95fe7ac0bb63ec554dd6d09a587c1e10963
Emiliano <emiliano.hotes@despegar.com>
2022-10-15 20:21:23 -0300

    added bump animation to button

--- food-delivery-app/src/components/Layout/HeaderCartButton.tsx
+++ food-delivery-app/src/components/Layout/HeaderCartButton.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.scss";
@@ -9,13 +9,33 @@ type Props = {
 
 const HeaderCartButton = ({ onClick }: Props) => {
   const cartCtx = useContext(CartContext);
+  const [btnBump, setBtnBump] = useState(false);
+
+  const { items } = cartCtx;
 
   const numberOfCartItems = cartCtx.items.reduce((acc, item) => {
     return acc + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnBump ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnBump(true);
+
+    const timer = setTimeout(() => {
+      setBtnBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
