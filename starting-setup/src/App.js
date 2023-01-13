0586fbb77cf000d2e32c69e7302cb1791cd9d6a6
Emiliano <emiliano.hotes@despegar.com>
2023-01-13 10:50:45 -0300

    added toggle paragraph functionality

--- starting-setup/src/App.js
+++ starting-setup/src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+    const [showParagraph, setShowParagraph] = useState(false);
+
+    console.log("APP RUNNING");
+
+    const toggleParagraphHandler = () => {
+        setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    };
+
+    return (
+        <div className="app">
+            <h1>Hi there!</h1>
+            {showParagraph && <p>This is new!</p>}
+            <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+        </div>
+    );
 }
 
 export default App;
