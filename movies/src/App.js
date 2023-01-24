cb678b32c472c2dc6b5bc8f0d2eeb1f633b48288
Emiliano <emiliano.hotes@despegar.com>
2023-01-24 18:36:26 -0300

    created AddMovie component and added firebase conection

--- movies/src/App.js
+++ movies/src/App.js
@@ -2,7 +2,10 @@ import { useState, useEffect, useCallback } from "react";
 import MoviesList from "./components/MoviesList";
 
 import "./App.css";
-const SWAPI_URL = "https://swapi.dev/api/films";
+import AddMovie from "./components/AddMovie";
+
+const FIREBASE_URL =
+    "https://movies-5c01c-default-rtdb.firebaseio.com/movies.json";
 
 function App() {
     const [movies, setMovies] = useState([]);
@@ -13,22 +16,26 @@ function App() {
         setIsLoading(true);
         setError(null);
         try {
-            const response = await fetch(SWAPI_URL);
+            const response = await fetch(FIREBASE_URL);
+
             if (!response.ok) {
                 throw new Error("Somethin went wrong!!");
             }
 
             const data = await response.json();
 
-            const transformedMovies = data.results.map((movieData) => {
-                return {
-                    id: movieData.episode_id,
-                    title: movieData.title,
-                    openingText: movieData.opening_crawl,
-                    releaseDate: movieData.release_date,
-                };
-            });
-            setMovies(transformedMovies);
+            const loadedMovies = [];
+
+            for (const key in data) {
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingText: data[key].openingText,
+                    releaseDate: data[key].releaseDate,
+                });
+            }
+
+            setMovies(loadedMovies);
         } catch (error) {
             setError(error);
         }
@@ -38,6 +45,16 @@ function App() {
     useEffect(() => {
         fetchMoviesHandler();
     }, [fetchMoviesHandler]);
+
+    async function addMovieHandler(movie) {
+        const response = await fetch(FIREBASE_URL, {
+            method: "POST",
+            body: JSON.stringify(movie),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    }
 
     let content = <p>Found no movies</p>;
 
@@ -55,6 +72,9 @@ function App() {
 
     return (
         <div className="App">
+            <section>
+                <AddMovie onAddMovie={addMovieHandler} />
+            </section>{" "}
             <section>
                 <h1>Movies list</h1>
                 <button onClick={fetchMoviesHandler}>Fetch Movies</button>
