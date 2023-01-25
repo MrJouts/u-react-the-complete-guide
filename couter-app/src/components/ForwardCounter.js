b9d131fa552739a9dc568a669ff0b47defeca437
Emiliano <emiliano.hotes@despegar.com>
2023-01-25 15:13:34 -0300

    added first custom hook - useCounter

--- couter-app/src/components/ForwardCounter.js
+++ couter-app/src/components/ForwardCounter.js
@@ -1,19 +1,11 @@
-import { useState, useEffect } from 'react';
+import Card from "./Card";
 
-import Card from './Card';
+import useCounter from "../hooks/use-counter";
 
 const ForwardCounter = () => {
-  const [counter, setCounter] = useState(0);
+    const counter = useCounter();
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return <Card>{counter}</Card>;
+    return <Card>{counter}</Card>;
 };
 
 export default ForwardCounter;
