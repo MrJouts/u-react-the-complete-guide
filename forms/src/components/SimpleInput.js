d70805aee2829ab3daa27a44110f7563c55d09d2
Emiliano <emiliano.hotes@despegar.com>
2023-01-27 12:51:06 -0300

    added form logic to SimpleInput

--- forms/src/components/SimpleInput.js
+++ forms/src/components/SimpleInput.js
@@ -1,15 +1,34 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+    const [enteredName, setEnteredName] = useState("");
+
+    const handleEnteredName = (event) => {
+        setEnteredName(event.target.value);
+    };
+
+    const formSubmissionHandler = (event) => {
+        event.preventDefault();
+        console.log(enteredName);
+        setEnteredName("");
+    };
+
+    return (
+        <form onSubmit={formSubmissionHandler}>
+            <div className="form-control">
+                <label htmlFor="name">Your Name</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={enteredName}
+                    onChange={handleEnteredName}
+                />
+            </div>
+            <div className="form-actions">
+                <button>Submit</button>
+            </div>
+        </form>
+    );
 };
 
 export default SimpleInput;
