714cf23f86770bb3884c1e94acde729cbc91414c
Emiliano <emiliano.hotes@despegar.com>
2023-02-01 09:51:36 -0300

    [food-app] Fetching meals via http

--- food-delivery-app/src/App.tsx
+++ food-delivery-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { CartItem, meal } from "./types";
 import { DUMMY_MEALS } from "./mocks/meals";
@@ -9,46 +9,70 @@ import Meals from "./components/Meals/Meals";
 import CartProvider from "./store/CartProvider";
 
 function App() {
-  const [meals, setMeals] = useState<meal[]>(DUMMY_MEALS);
-  const [cartItems, setCartItems] = useState<CartItem[] | null>(null);
-  const [cartIsShown, setCartIsShown] = useState<boolean>(false);
+    const [meals, setMeals] = useState<meal[]>([]);
+    const [cartItems, setCartItems] = useState<CartItem[] | null>(null);
+    const [cartIsShown, setCartIsShown] = useState<boolean>(false);
 
-  const addMealToCart = (amount: number, name: string, price: number) => {
-    const newCartItem: CartItem = {
-      name,
-      amount: +amount,
-      price: price,
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch(
+                "https://food-app-5531c-default-rtdb.firebaseio.com/meals.json"
+            );
+            const responseData = await response.json();
+
+            const loadedMeals = [];
+
+            for (const key in responseData) {
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price,
+                });
+            }
+
+            setMeals(loadedMeals);
+        };
+
+        fetchMeals();
+    }, []);
+
+    const addMealToCart = (amount: number, name: string, price: number) => {
+        const newCartItem: CartItem = {
+            name,
+            amount: +amount,
+            price: price,
+        };
+
+        let newItems: CartItem[] = [];
+
+        if (cartItems === null) {
+            newItems = [newCartItem];
+        } else if (cartItems.find((cart) => cart.name == name)) {
+            newItems = cartItems.map((cart) => {
+                if (cart.name == name) {
+                    cart.amount += +amount;
+                }
+                return cart;
+            });
+        } else {
+            newItems = cartItems.concat(newCartItem);
+        }
+
+        setCartItems((prevItems) => (prevItems = newItems));
     };
 
-    let newItems: CartItem[] = [];
+    const showCart = () => setCartIsShown(true);
 
-    if (cartItems === null) {
-      newItems = [newCartItem];
-    } else if (cartItems.find((cart) => cart.name == name)) {
-      newItems = cartItems.map((cart) => {
-        if (cart.name == name) {
-          cart.amount += +amount;
-        }
-        return cart;
-      });
-    } else {
-      newItems = cartItems.concat(newCartItem);
-    }
+    const hideCart = () => setCartIsShown(false);
 
-    setCartItems((prevItems) => (prevItems = newItems));
-  };
-
-  const showCart = () => setCartIsShown(true);
-
-  const hideCart = () => setCartIsShown(false);
-
-  return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCart} cartItems={cartItems} />}
-      <Header cartItems={cartItems} onShowCart={showCart} />
-      <Meals meals={meals} addMealToCart={addMealToCart} />
-    </CartProvider>
-  );
+    return (
+        <CartProvider>
+            {cartIsShown && <Cart onClose={hideCart} cartItems={cartItems} />}
+            <Header cartItems={cartItems} onShowCart={showCart} />
+            <Meals meals={meals} addMealToCart={addMealToCart} />
+        </CartProvider>
+    );
 }
 
 export default App;
